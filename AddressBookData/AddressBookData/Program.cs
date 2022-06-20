2ece15a73396cc829a31ac8ed315134bd2ac572a
mohan3388 <sahumohan338@gmail.com>
2022-06-20 02:55:48 -0700

    [mohan]add: create data table in Address book

---
+++ AddressBookData/AddressBookData/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AddressBookData;
+
+namespace AddressBookData
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBook.CreateTableUsingLinq();
+
+        }
+    }
+}
